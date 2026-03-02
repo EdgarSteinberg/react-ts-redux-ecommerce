8bd270a307633c4ca3f13a80abe1af4d0f4243c2
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-03-02 12:32:23 -0300

    feat(swagger) agregando link de documentacion de la api

--- src/components/navBar/adminNavBar.tsx
+++ src/components/navBar/adminNavBar.tsx
@@ -5,16 +5,23 @@ import { Link } from "react-router-dom";
 
 
 const AdminNavBar = () => {
-
+    const DOCS_URL = "http://localhost:8080/api/docs";
     return (
         <Navbar bg="secondary" variant="dark">
-            <Container style={{height: '16px'}}>
+            <Container style={{ height: '16px' }}>
                 <Navbar.Brand>Admin</Navbar.Brand>
 
                 <Nav>
                     <Nav.Link as={Link} to="/admin/products">Productos</Nav.Link>
                     <Nav.Link as={Link} to="/admin/products/new">Nuevo</Nav.Link>
                     <Nav.Link as={Link} to="/admin/users">Usuarios</Nav.Link>
+                    <Nav.Link
+                        href={DOCS_URL}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Docs
+                    </Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
